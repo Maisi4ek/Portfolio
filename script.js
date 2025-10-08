4e18339df6eecfb929d554afada403784f9aeaed
Maisi4ek <klimka.paltarzhytski@gmail.com>
2025-10-08 17:05:21 -0400

    Added Works Section

---
+++ script.js
@@ -0,0 +1,16 @@
+const sections = Array.from(document.querySelectorAll("section"));
+let active = document.querySelector(".active");
+const buttons = document.querySelectorAll(".link");
+
+window.addEventListener("scroll", function () {
+    const position = window.scrollY
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop;
+        const sectionBottom = sectionTop + section.offsetHeight;
+        if (sectionTop <= position && position < sectionBottom) {
+            const index = sections.indexOf(section);
+            console.log(index);
+        }
+    }
+})
